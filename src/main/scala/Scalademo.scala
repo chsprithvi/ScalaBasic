aef13172c527a9d73a51446f6e91eab9320136a8
chsprithvi <chsprithvi@gmail.com>
2020-09-25 09:33:16 +0530

    [Siddhartha]Added:Match expr, for, loops

--- src/main/scala/Scalademo.scala
+++ src/main/scala/Scalademo.scala
@@ -1,15 +1,20 @@
 object Scalademo {
   def main(args: Array[String]): Unit = {
-    var a: Int = 10
-    val name = "mark"
-    val age = 22
-    print("\nvalue of a=" + a)
-    a = 22
-    print("\nvalue of a=" + a)
-    println("\n" + name + " is " + age + "years.") //print
-    println(s"\n $name is $age years.") //with String Interpolation
-    println(f"\n $name%s is $age%d years.") //with type safe
-    println(s"\nHello world")
-    println(raw"\nhello world")
+    val age =2
+    for(count <- 1 to 5){
+      println(count)
+    }
+    val list = List(1,2,3,4,5,6,76,43)
+    for(travel<-list; if travel<10){
+      print(+travel+"\t")
+    }
+    val result=for{travel<-list; if travel<6} yield {travel*2}
+    print("\n"+result)
+    //matching expressions
+    age match{
+      case 20 => println(age)
+      case _ => println("error")
+    }
+
   }
 }
